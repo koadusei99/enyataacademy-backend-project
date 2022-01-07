cc41f72cb1c6c9c3872d87b6c40c3911247bd12f
Maampeaw <markamoah97@gmail.com>
2022-01-07 14:33:00 +0000

    update

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require("express");
 const { registerUser } = require("./controllers/AuthController");
 const app = express();
 const port = 3000;
-const {getUserInfo} = require('./authController.js')
+const {RegisterValidator} = require('./validators.js')
+
 
 app.use(express.json())
 
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/register", registerUser);
+app.post("/register", RegisterValidator, registerUser);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,13 @@
+const { validationResult } = require("express-validator");
+const { errorFormatter } = require("../validators.js");
+
+
 //TODO register controller
 const registerUser = (req, res) => {
+  const errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.mapped() });
+  }
   const { email, password, firstName, lastName } = req.body;
   let user = { email, password, firstName, lastName };
   let token = "kudhkdsunds89sdyisud";

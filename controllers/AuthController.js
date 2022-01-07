8b637bf1670c20b0e195415fe2895c408b493cd8
pyaaddo <pyaaddo@gmail.com>
2022-01-07 14:40:50 +0000

    dlt

--- controllers/AuthController.js
+++
@@ -1,9 +0,0 @@
-//TODO register controller
-const registerUser = (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
-  let user = { email, password, firstName, lastName };
-  let token = "kudhkdsunds89sdyisud";
-  res.status(201).json({ message: "Account Created", token });
-};
-
-module.exports = { registerUser };
--- middleware/Auth.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-
-function genToken(email, firstName) {
-  return jwt.sign({ email, firstName }, process.env.TOKEN_SECRET, {
-    expiresIn: "1800s",
-  });
-}

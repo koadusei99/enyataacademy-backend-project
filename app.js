e0c255e1299f02d400de7dfde9985d4c469e8b49
DTGlov <67828079+JaeGlo@users.noreply.github.com>
2022-01-07 12:50:37 +0000

    completed database integration

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const morgan = require('morgan');
+var pg = require('pg');
+
 const app = express();
+require('dotenv').config();
 const port = 3000;
+
+
+
+app.use(morgan('dev'));
+
+var conString = process.env.DATABASE_URL;
+var client = new pg.Client(conString);
+
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    console.log('DB Connected Successfully!');
+    client.end();
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
